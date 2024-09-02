d67f008eb57a12157b1cc0c4d32686e36e9a28ff
Vaibhav Holani <vaibhav.holani@dconstruct.ai>
2024-09-02 15:11:13 -0400

    added new setup info

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="count_rasterizer",
+    packages=['count_rasterizer'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="count_rasterizer._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
